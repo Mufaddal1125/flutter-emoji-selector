09371b00de64e79ec70946b070c90e6f8b1e7ad5
Mufaddal Patanwala <65411218+Mufaddal1125@users.noreply.github.com>
2023-07-01 16:07:46 +0400

    use popup menu for showing skin emojis

--- lib/src/emoji_widget.dart
+++ lib/src/emoji_widget.dart
@@ -1,6 +1,5 @@
 import 'package:emojis/emoji.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_portal/flutter_portal.dart';
 
 class EmojiWidget extends StatefulWidget {
   const EmojiWidget({
@@ -24,79 +23,81 @@ class EmojiWidget extends StatefulWidget {
 }
 
 class _EmojiWidgetState extends State<EmojiWidget> {
-  /// whether to show popup
-  bool _pickingSkinTone = false;
+  final _popupKey = GlobalKey<PopupMenuButtonState>();
 
   @override
   Widget build(BuildContext context) {
-    return PortalTarget(
-      visible: _pickingSkinTone,
-      portalFollower: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => setState(() => _pickingSkinTone = false),
-      ),
-      child: PortalTarget(
-        anchor: const Aligned(
-          follower: Alignment.bottomCenter,
-          target: Alignment.topCenter,
-          backup: Aligned(
-            follower: Alignment.bottomLeft,
-            target: Alignment.topRight,
-            offset: Offset(-45.0, 0.0),
-            backup: Aligned(
-              offset: Offset(45.0, 0.0),
-              follower: Alignment.bottomRight,
-              target: Alignment.topLeft,
-            ),
-          ),
-        ),
-        visible: _pickingSkinTone,
-        portalFollower: LayoutBuilder(builder: (context, constraints) {
-          return SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
-            child: Card(
-              elevation: 3,
-              child: Wrap(
-                runAlignment: WrapAlignment.center,
-                alignment: WrapAlignment.center,
-                children: [
-                  for (var emoji in widget.modifiablEmojis!)
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: InkWell(
-                        onTap: () {
-                          widget.onEmojiSelected?.call(emoji);
-                          setState(() => _pickingSkinTone = false);
-                        },
-                        child: Text(
-                          emoji.char,
-                          style: const TextStyle(fontSize: 30),
-                        ),
-                      ),
-                    )
-                ],
-              ),
-            ),
-          );
-        }),
-        child: Center(
-          child: InkWell(
-            onTap: () => widget.onEmojiSelected?.call(widget.item),
-            onDoubleTap: () {
-              if (widget.modifiablEmojis != null) {
-                setState(() => _pickingSkinTone = !_pickingSkinTone);
-              }
-            },
-            child: Tooltip(
-              message: widget.item.shortName.replaceAll('_', ' '),
-              child: Text(
-                widget.item.char,
-                style: const TextStyle(fontSize: 30),
-              ),
+    return _popupMenuWrapper(
+      child: Center(
+        child: InkWell(
+          onTap: () => widget.onEmojiSelected?.call(widget.item),
+          onDoubleTap: showSkinToneSelector,
+          onLongPress: showSkinToneSelector,
+          onSecondaryTap: showSkinToneSelector,
+          child: Tooltip(
+            message: widget.item.shortName.replaceAll('_', ' '),
+            child: Text(
+              widget.item.char,
+              style: const TextStyle(fontSize: 30),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  void showSkinToneSelector() {
+    if (widget.modifiablEmojis == null) return;
+    _popupKey.currentState?.showButtonMenu();
+  }
+
+  void closeSkinToneSelector() {
+    if (widget.modifiablEmojis == null) return;
+    Navigator.of(context).pop();
+  }
+
+  Widget _buildSkinEmojis() {
+    return PopupMenuTheme(
+      data: const PopupMenuThemeData(
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      child: Card(
+        elevation: 0,
+        child: Wrap(
+          runAlignment: WrapAlignment.center,
+          alignment: WrapAlignment.center,
+          children: [
+            for (var emoji in widget.modifiablEmojis!)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: InkWell(
+                  onTap: () {
+                    widget.onEmojiSelected?.call(emoji);
+                    closeSkinToneSelector();
+                  },
+                  child: Text(
+                    emoji.char,
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _popupMenuWrapper({required Widget child}) {
+    if (widget.modifiablEmojis == null) return child;
+    return PopupMenuButton(
+      key: _popupKey,
+      enabled: false,
+      itemBuilder: (context) =>
+          [PopupMenuItem(enabled: false, child: _buildSkinEmojis())],
+      child: child,
     );
   }
 }
